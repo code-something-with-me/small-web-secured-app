4b3d61ccd20b59d8214736527f1894466ee59607
安栋 <andong@xiaomalixing.com>
2023-04-23 16:39:54 +0800

    init database with docker compose

--- sql/dump.sql
+++ sql/dump.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA `spring` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
 CREATE TABLE IF NOT EXISTS `spring`.`user`
 (
     `id`        INT         NOT NULL AUTO_INCREMENT,
@@ -23,3 +25,12 @@ CREATE TABLE IF NOT EXISTS `spring`.`product`
     `currency` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+INSERT IGNORE INTO `spring`.`user` (`id`, `username`, `password`, `algorithm`)
+VALUES ('1', 'john', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+INSERT IGNORE INTO `spring`.`authority` (`id`, `name`, `user`)
+VALUES ('1', 'READ', '1');
+INSERT IGNORE INTO `spring`.`authority` (`id`, `name`, `user`)
+VALUES ('2', 'WRITE', '1');
+INSERT IGNORE INTO `spring`.`product` (`id`, `name`, `price`, `currency`)
+VALUES ('1', 'Chocolate', '10', 'USD');
